79558a24b84b9d58baee5b8d504e692bfc83c4f0
alinovskiy <a.alinovskiy@tsconsulting.com>
2021-09-30 19:21:33 +0300

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './styles.css';
-import IMAGE from './logo.png';
+import React from 'react'
+import './styles.css'
+import IMAGE from './logo.png'
 import LOGO from './logo.svg'
-import {ClickCounter} from "./ClickCounter";
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    return (
-        <>
-            <h1>Typescript1 - {process.env.NODE_ENV} {process.env.name}</h1>
-            <ClickCounter/>
-            <img src={IMAGE} alt="logo" width="300" height="300"/>
-            <img src={LOGO} alt="logo" width="300"/>
-        </>
-    );
-};
+  return (
+    <>
+      <h1>
+        Typescript1 - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <ClickCounter />
+      <img src={IMAGE} alt="logo" width="300" height="300" />
+      <img src={LOGO} alt="logo" width="300" />
+    </>
+  )
+}
